d973ece1ca5c2004571ed44198d6affc8b321809
joaodiasrosa <154565824+joaodiasrosa@users.noreply.github.com>
2025-06-30 21:01:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirFormulario() {
+  window.location.href = "formulario.html";
+}
+
+function abrirFrutas() {
+  window.location.href = "frutas.html";
+}
+
+function abrirJogo() {
+  window.location.href = "jogo.html";
+}
